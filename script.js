52f1216e42238e8a393a2f5d48d108c2371bfd96
ayushDtu <ayushjha_mc22a13_64@dtu.ac.in>
2024-05-29 21:02:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,4 @@ const openInNewWindow = (url) => {
     window.open(url, '_blank');
 }
 
-import AOS from "aos";
-import "aos/dist/aos.css";
 
-// init AOS animation
-AOS.init({
-    duration: 1000,
-    offset: 100,
-});
