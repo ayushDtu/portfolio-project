4471234ece55e2a6d2dc06432ddf86593070a259
ayushDtu <ayushjha_mc22a13_64@dtu.ac.in>
2024-05-29 20:57:32 +0530

    29/05/24 8:57pm

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ const toggleMenu = () => {
 const openInNewWindow = (url) => {
     window.open(url, '_blank');
 }
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
